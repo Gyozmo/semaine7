30ba91185beb81cac22fd0b2c5f35409cb1bad3a
Gyozmo <binantguillaume@gmail.com>
2018-05-31 15:53:04 +0200

    working function getJSON

--- Trouve ma ville/main.js
+++ Trouve ma ville/main.js
@@ -1,10 +1,15 @@
+let weather = "api.openweathermap.org/data/2.5/weather?q=Toulouse,fr&appid=d58b214415a07a0b03c33ff4a8eb49e1"
+// $.ajax({
+//     url: "api.openweathermap.org/data/2.5/weather?q=Toulouse,fr&appid=d58b214415a07a0b03c33ff4a8eb49e1",
+//     data: {
+//       zipcode: 97201
+//     },
+//     success: function( result ) {
+//       $( "h1" ).after( "<strong>" + result.weather + "</strong> degrees" );
+//     }
+//   });
 
-$.ajax({
-    url: "/api/getWeather",
-    data: {
-      zipcode: 97201
-    },
-    success: function( result ) {
-      $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
-    }
-  });
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Toulouse,fr&appid=d58b214415a07a0b03c33ff4a8eb49e1',function(data){
+    console.log(data.main.temp_max);
+    
+})
